8ad2f872adea838bbc6c5e20a0a42180f87cdb3e
Artem Tkachenko <shikams303@gmail.com>
2025-07-13 14:16:22 +0300

    added community page and separate nav-link for 'use-client'

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,29 +3,30 @@ import Image from "next/image";
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 import MainHeaderBackGround from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
     <>
-    <MainHeaderBackGround />
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="a plate with food" priority />
-        NextLevel Food
-      </Link>
+      <MainHeaderBackGround />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="a plate with food" priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+              
+            </li>
+            <li>
+              <NavLink href="/community">Foodies community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
-  
 }
