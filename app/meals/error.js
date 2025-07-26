32d6148238cd8c9b0129ac92a3794d753956e11d
Artem Tkachenko <shikams303@gmail.com>
2025-07-26 11:47:22 +0300

    error handling

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+'use client'
+
+export default function Error({error}) {
+    return <main className="error">
+        <h1>an error occured</h1>
+        <p>failed to fetch meal data</p>
+    </main>
+}
